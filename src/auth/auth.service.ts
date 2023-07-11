3ca9c35d14a14b1435dc7b7c06394f718b8fbedf
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-07-11 16:31:10 +0600

    implemented auth login by retrieving user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { UserSignUpDto } from './dtos/user-signup.dto';
 import { UserLoginDto } from './dtos/user-login.dto';
+import { User } from './models/user.model';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
   signUp(userSignUpDto: UserSignUpDto) {}
 
-  login(userLoginDto: UserLoginDto) {}
+  async login(userLoginDto: UserLoginDto) {
+    // retrieve user
+    const { email, password } = userLoginDto;
+    const user = await this.userModel.findOne({ email }).exec();
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+  }
 }
